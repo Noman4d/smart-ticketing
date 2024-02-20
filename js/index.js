50710f9eb49dc8954edc798143e79a0b76546589
Noman4d <nomanahmed.abdullah@gmail.com>
2024-02-20 22:17:22 +0600

    added tikcetCounting

--- js/index.js
+++ js/index.js
@@ -1,31 +1,45 @@
-const allBtn = document.getElementsByClassName('btn-select');
+const allBtn = document.getElementsByClassName("btn-select");
 
-let leftSeat = 40 ; 
+let leftSeat = 40;
 let addSeat = 0;
 
 for (const btn of allBtn) {
-    btn.addEventListener('click',function () {
-        const btnColor = "#1DD100";
-        btn.style.backgroundColor = btnColor;
-        leftSeat = leftSeat - 1 ; 
-        const leftseats = document.getElementById('left-seat');
-        leftseats.innerText=leftSeat;
-        addSeat = addSeat + 1;
-        document.getElementById('add-seat').innerText=addSeat;
-        
-        const seatNumber = btn.innerText;
+  btn.addEventListener("click", function () {
+    const btnColor = "#1DD100";
+    btn.style.backgroundColor = btnColor;
+    leftSeat = leftSeat - 1;
+    const leftseats = document.getElementById("left-seat");
+    leftseats.innerText = leftSeat;
+    addSeat = addSeat + 1;
+    document.getElementById("add-seat").innerText = addSeat;
 
-        const p1 = document.createElement('p');
-        p1.innerText = 'Economoy'
-        console.log(p1.innerText);
-    })
+    const ulConteiner = document.getElementById("selected-conteiner");
+
+    const seatNumber = btn.innerText;
+    const p1 = document.createElement("p");
+    p1.innerText = seatNumber;
+
+    const p2 = document.createElement("p");
+    p2.innerText = "Economoy";
+    const p3 = document.createElement("p");
+    p3.innerText = "550";
+    const converTedp3 = parseInt(p2);
+
+    const ticketConteiner = document.createElement("li");
+
+    ticketConteiner.appendChild(p1);
+    ticketConteiner.appendChild(p2);
+    ticketConteiner.appendChild(p3);
+
+    ulConteiner.appendChild(ticketConteiner);
+  });
 }
 
 // scroll down //
 
-const scroll = document.getElementById('scroll-down');
+const scroll = document.getElementById("scroll-down");
 
-scroll.addEventListener('click',function () {
-    const scrollTargett=document.getElementById('scroll-target');
-    scrollTargett.scrollIntoView({ behavior: 'smooth' });
-})
+scroll.addEventListener("click", function () {
+  const scrollTargett = document.getElementById("scroll-target");
+  scrollTargett.scrollIntoView({ behavior: "smooth" });
+});
